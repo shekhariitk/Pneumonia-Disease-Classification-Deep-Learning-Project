14e9a6ccd1d4c94829da5053ac1790a3be99b6e0
Shekhar Suman <s.sumanpathak513@gmail.com>
2025-06-14 21:20:15 +0530

    module error handle

--- setup.py
+++ setup.py
@@ -1,31 +1,41 @@
-import setuptools
+
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 REPO_NAME = "Pneumonia-Disease-Classification-Deep-Learning-Project"
 AUTHOR_NAME = "Shekhar"
-AUTHOR_USER_NAME = "shekhariitk"
-SRC_REPO = "pneumonia_detection"
 AUTHOR_EMAIL = "s.sumanpathak513@gmail.com"
 
 
-setuptools.setup(
-    name=SRC_REPO,
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [ req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+        return requirements
+
+        
+
+
+
+setup(
+    name=REPO_NAME,
     version=__version__,
     author=AUTHOR_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
-    long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
-
+    install_requires  = get_requirements("requirements.txt"),
+    packages=find_packages()
 )
--- src/logger.py
+++ src/logger.py
@@ -16,9 +16,5 @@ LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 logging.basicConfig(
     filename= LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-    handlers=[
-        logging.FileHandler(LOG_FILE_PATH, mode='w'),
-        logging.StreamHandler(sys.stdout)
-    ]
+    level=logging.INFO
 )
